6cb16e91f7def35577516761dc475e88e627ba8e
Guilherme Cunha <guicunhaalves@gmail.com>
2025-11-05 22:05:11 -0300

    add endereco, numero e nome do negocio + id do servico

--- src/main/java/com/java360/agendei/infrastructure/dto/AgendamentoDTO.java
+++ src/main/java/com/java360/agendei/infrastructure/dto/AgendamentoDTO.java
@@ -18,12 +18,18 @@ public class AgendamentoDTO {
     private String clienteNome;
     private String prestadorNome;
     private String servicoTitulo;
+    private Integer servicoId;
     private String enderecoNegocio;
+    private String numeroNegocio;
+    private String nomeNegocio;
     private LocalDateTime dataHora;
     private StatusAgendamento status;
 
     public static AgendamentoDTO fromEntity(Agendamento agendamento) {
         String enderecoNegocio = null;
+        String numeroNegocio = null;
+        String nomeNegocio = null;
+
 
         // Evita nullpointer exception
         if (agendamento.getServico() != null && agendamento.getServico().getNegocio() != null) {
@@ -31,6 +37,8 @@ public class AgendamentoDTO {
             enderecoNegocio = negocio.getEndereco() != null
                     ? negocio.getEndereco().toString()
                     : null;
+            numeroNegocio = negocio.getNumero();
+            nomeNegocio = negocio.getNome();
         }
 
         return new AgendamentoDTO(
@@ -38,7 +46,10 @@ public class AgendamentoDTO {
                 agendamento.getCliente().getNome(),
                 agendamento.getPrestador().getNome(),
                 agendamento.getServico().getTitulo(),
+                agendamento.getServico().getId(),
                 enderecoNegocio,
+                numeroNegocio,
+                nomeNegocio,
                 agendamento.getDataHora(),
                 agendamento.getStatus()
         );
